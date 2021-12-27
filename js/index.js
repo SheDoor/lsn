557ad4fb28c7e039e4973c835020019dfc2fb4c2
犀盗 <shedoor@163.com>
2021-12-27 17:20:21 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||董|诗|琪|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3|祝|董|诗|琪|生|日|快|乐|董诗琪|生日快乐|');
     }
 
     S.Drawing.loop(function () {
